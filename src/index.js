e4a54228328cd91b1e1a6d514dfc1ca7a6e36218
AbiRodStr <abistrain23@gmail.com>
2025-01-05 13:07:23 +0100

    removed git attributes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-const button = document.querySelector("button#empowering");
-const showButton = document.querySelector("button#empowering + button");
-const closeButton = document.querySelector("button#empowering button");
-
-showButton.addEventListener("click", () => {
-  dialog.showModal();
-});
-
-closeButton.addEventListener("click", () => {
-  dialog.close();
-});
-
-const  = document.querySelector("dialog#academic");
-const showButton = document.querySelector("dialog#academic + button");
-const closeButton = document.querySelector("dialog#academic button");
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button#empowering");
+const closeButton = document.querySelector("dialog button");
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
